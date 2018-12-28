86fcf0162cdc0ddd1590063bccd0bb13f1883dbf
liuhao <alan.liu@yiako.com>
2018-12-28 15:48:26 +0800

    init activiti

---
+++ src/main/java/self/activiti/dao/TestMapper.java
@@ -0,0 +1,9 @@
+package self.activiti.dao;
+
+/**
+ * @Author: liuhao
+ * @Description:
+ * @Date: Create in 6:11 PM 2018/12/27
+ */
+public interface TestMapper {
+}
---
+++ src/test/java/self/activiti/ActivitiApplicationTests.java
@@ -0,0 +1,17 @@
+package self.activiti;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ActivitiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
