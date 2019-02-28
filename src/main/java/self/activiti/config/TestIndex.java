3bf139b3bd38a8d4935f76078eb685a7667a416a
liuhao <alan.liu@yiako.com>
2019-02-28 15:10:45 +0800

    test not add

---
+++ src/main/java/self/activiti/config/TestIndex.java
@@ -0,0 +1,12 @@
+package self.activiti.config;
+
+/**
+ * @Author: liuhao
+ * @Description:
+ * @Date: Create in 3:00 PM 2019/2/28
+ */
+public class TestIndex {
+    public static void main(String[] args) {
+
+    }
+}
